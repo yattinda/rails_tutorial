19e4d9142b4729ee6cadae4449af3bfa39aecaf0
yattinda <yattinda@gmail.com>
2021-04-10 18:19:37 +0900

    Fix indent

--- environment/sample_app/app/controllers/application_controller.rb
+++ environment/sample_app/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  render html: "hello, world!"
-end
+    render html: "hello, world!"
+  end
+
 end
