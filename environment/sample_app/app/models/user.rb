a6978521571339fdb479c179de9d9d28e5146e55
yattinda <yattinda@gmail.com>
2021-05-16 22:13:26 +0900

    Uniqueness

--- environment/sample_app/app/models/user.rb
+++ environment/sample_app/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  #self.email.downcaseのselfは省略可
+  #before_save { email.downcase! } ともかける
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   #大文字は定数
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
