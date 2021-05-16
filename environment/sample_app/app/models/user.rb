036cd11ccd6fdf62baf15f674b4d510c28a395b9
yattinda <yattinda@gmail.com>
2021-05-16 22:26:29 +0900

    Fix

--- environment/sample_app/app/models/user.rb
+++ environment/sample_app/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   #大文字は定数
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX }
+    format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
   has_secure_password
 end
