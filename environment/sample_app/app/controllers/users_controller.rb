80aabd340fc08f2a925f2a62be85fef21ebda8db
yattinda <yattinda@gmail.com>
2021-05-20 23:52:35 +0900

    Use strong parameters

--- environment/sample_app/app/controllers/users_controller.rb
+++ environment/sample_app/app/controllers/users_controller.rb
@@ -8,4 +8,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
